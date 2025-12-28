c7bd5cfa70e9f514c4f4f5879950da1ed45169f3
winkangwin <2916305402@qq.com>
2025-12-28 16:58:46 +0800

    12.28

---
+++ test_12_26/test_12_26/test_12_26.c
@@ -0,0 +1,204 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int num1 = scanf("%d", &a);
+//	int b = a - 5;
+//	printf("%d\n", b);
+//	printf("%d\n", num1);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[20];//数组
+//	scanf("%s", arr);//arr没有取地址，也不需要地址，因为数组名其实就是地址
+//	printf("%s\n", arr);
+//	return 0;
+//
+//}
+
+
+//int main()
+//{
+//	char arr[5];
+//
+//	scanf("%4s", arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[5];
+//
+//	scanf("%[^\n]s", arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d,%d", &a, &b);
+//
+//	printf("a = %d b = %d\n", a, b);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	int day = 0;
+//	scanf("%d%*c%d%*c%d", &year, &month, &day);
+//	printf("%d-%d-%d", year, month, day);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//
+//	if (n % 2 == 1) //== 是判断相等, = 是赋值
+//	{
+//		printf("%d 是奇数\n", n);
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//
+//	if (num % 2 == 1)
+//		printf("%d 是奇数\n", num);
+//	else
+//		printf("%d 是偶数\n", num);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int age = 0;
+//	printf("请输入你的年龄：");
+//	scanf("%d", &age);
+//
+//	if (age >= 18)
+//		printf("成年\n");
+//	else
+//		printf("未成年");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//
+//	if (num == 0)
+//		printf("是0\n");
+//	else if (num > 0)
+//		printf("正数\n");
+//	else
+//		printf("负数\n");
+//
+//	
+// return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d", &a);
+//
+//	b = (a > 5 ? 3 : -3);
+//
+//	printf("%d\n", b);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int flag = 0;
+//	scanf("%d", &flag);
+//
+//	if (!flag)
+//		printf("啊哈哈\n");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int month = 0;
+//	scanf("%d", &month);
+//	if (month >= 3 && month <= 5)
+//	{
+//		printf("春季\n");
+//	}
+//	else
+//	{
+//		printf("该月份不是春季\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int month = 0;
+//	scanf("%d", &month);
+//	if (month == 12 || month == 1 || month == 2)
+//	{
+//		printf("该月份为冬季");
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int year = 0;
+//	scanf("%d", &year);
+//
+//	if (year % 4 == 0 && year % 100 != 0)
+//	{
+//		printf("该年份是闰年\n");
+//	}
+//	else if (year % 400 == 0)
+//	{
+//		printf("该年份是闰年\n");
+//	}
+//	else
+//	{
+//		printf("该年份不是闰年\n");
+//	}
+//	return 0;
+//}
+
+
